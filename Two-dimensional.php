818ef607b9c2262b8b438dfeb815d6f45d8826b1
usmanit <usmanbcl@gmail.com>
2017-09-12 21:15:04 -0700

    latihan array

---
+++ Two-dimensional.php
@@ -0,0 +1,15 @@
+<?php
+$hp = array
+(
+	array("asus",33,19),
+	array("nokia",39,32),
+	array("lenovo",23,21),
+	array("oppo",22,12),
+	);
+	
+	echo $hp[0][0].": In stock: ".$hp[0][1].", sold: ".$hp[0][2].".<br>";
+	echo $hp[1][0].": In stock: ".$hp[1][1].", sold: ".$hp[1][2].".<br>";
+	echo $hp[2][0].": In stock: ".$hp[2][1].", sold: ".$hp[2][2].".<br>";
+	echo $hp[3][0].": In stock: ".$hp[3][1].", sold: ".$hp[3][2].".<br>"; 
+	
+?>
